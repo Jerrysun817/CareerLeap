a3f7d37e0d5bad68c0bd0d20534abc936b2aa6bd
Jerrysun817 <sunyiran0817@gmail.com>
2022-11-06 16:49:11 -0800

    Jerry's version

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Company from './Company';
+import Home from './Home'
+import Header from './Header';
+import FilterBy from './FilterBy';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="FilterBy" element={<FilterBy />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/FilterBy.js
@@ -0,0 +1,107 @@
+import React, { useState } from "react";
+import Company from "./Company";
+import data from "./data.json";
+import { Link, useNavigate } from "react-router-dom";
+
+
+function FilterBy() {
+
+    const getDisabilityInitialState = () => {
+        const disability = "Yes";
+        return disability;
+    };
+
+    const getJobInitialState = () => {
+        const job = "Software";
+        return job;
+    };
+    
+    const [disability, setDisability] = useState(getDisabilityInitialState);
+    const [job, setJob] = useState(getJobInitialState);
+    let [result, setResult] = useState([]);
+
+    const handleDisabilityChange = (e) => {
+        setDisability(e.target.value);
+    };
+
+    const handleJobChange = (e) => {
+        setJob(e.target.value);
+    };
+
+    const navigate = useNavigate();
+  
+    const navigateToHome = () => {
+      //  navigate back to Home
+        navigate("/");
+    };
+    
+    function handleFilter() {
+        let filtered = data.filter(item => item.tags.includes(disability, job));
+        setResult(filtered);
+        console.log(disability, job);
+        console.log(result);
+    }
+    
+    return (
+        <div>
+            <div>
+                <div>
+                <button onClick={navigateToHome}>go back</button>
+                </div>
+                {/* <Select options={disability} placeholder="Filter by Disability"/>
+                <Select options={jobs} placeholder="Filter by Job Type"/> */}
+                <select value={disability} onChange={handleDisabilityChange}>
+                    <option value="Yes">Yes</option>
+                    <option value="No">No</option>
+                </select>
+                <select value={job} onChange={handleJobChange}>
+                    <option value="Software">Software</option>
+                    <option value="Hardware">Hardware</option>
+                </select>
+                {/* <p>{`You selected ${disability}, ${job}`}</p> */}
+            </div>
+            <button onClick={() => handleFilter()}>Go</button>
+            <div>
+                {
+                    result.map((item) => {
+                        return (
+                            <Company image={item.image}
+                                    email={item.email}
+                                    linkedin={item.linkedin}
+                                    name={item.name}
+                                    location={item.location}
+                                    job={item.job}/>
+                        )
+                    })
+                }
+            </div>
+        </div>
+    );
+  }
+  
+  export default FilterBy;
+
+// import React from "react";
+// import Company from "./Company";
+// import data from "./data.json";
+// import { Link, useNavigate } from "react-router-dom";
+
+// function FilterBy() {
+//     const navigate = useNavigate();
+  
+//     const navigateToHome = () => {
+//       //  navigate back to Home
+//       navigate("/");
+//     };
+  
+//     return (
+//       <div>
+//         <div>
+//           <button onClick={navigateToHome}>go back</button>
+//         </div>
+
+//       </div>
+//     );
+//   }
+  
+//   export default FilterBy;
